33063b24cedd0f70ba976de71172d12a19987405
Shalin <shalins@infostretch.com>
2017-06-05 18:24:03 +0530

    added OAuthRestClient.java and related properties.
    set
    rest.client.oauth.auth.client_id,
    rest.client.oauth.auth.client_secret,
    rest.client.oauth.auth.username,
    rest.client.oauth.auth.password,
    rest.client.oauth.auth.grant_type,
    rest.client.oauth.auth.authentication_server_url

--- src/com/qmetry/qaf/automation/rest/client/BasicAuthRestClient.java
+++ src/com/qmetry/qaf/automation/rest/client/BasicAuthRestClient.java
@@ -7,15 +7,21 @@ import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 
 public class BasicAuthRestClient extends DefaultRestClient {
 
-	public static final String REST_CLIENT_BASIC_AUTH_USER = "rest.client.basic.auth.username";
-	public static final String REST_CLIENT_BASIC_AUTH_PASSWORD = "rest.client.basic.auth.password";
+	public static final String REST_CLIENT_BASIC_AUTH_USER =
+			"rest.client.basic.auth.username";
+	public static final String REST_CLIENT_BASIC_AUTH_PASSWORD =
+			"rest.client.basic.auth.password";
 
 	@Override
 	protected Client createClient() {
+
 		Client client = super.createClient();
-		client.addFilter(
-				new HTTPBasicAuthFilter(ConfigurationManager.getBundle().getString(REST_CLIENT_BASIC_AUTH_USER, ""),
-						ConfigurationManager.getBundle().getString(REST_CLIENT_BASIC_AUTH_PASSWORD, "")));
+		client.getProperties().put("jersey.config.client.followRedirects", true);
+		client.addFilter(new HTTPBasicAuthFilter(
+				ConfigurationManager.getBundle().getString(REST_CLIENT_BASIC_AUTH_USER,
+						""),
+				ConfigurationManager.getBundle()
+						.getString(REST_CLIENT_BASIC_AUTH_PASSWORD, "")));
 		return client;
 	}
 }
